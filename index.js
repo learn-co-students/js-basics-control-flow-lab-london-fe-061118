dc4f50746495819daa7739125dec5f2663a90813
Alysha Neeamuth <alyshanee@gmail.com>
2018-07-11 20:03:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your code in this file!
+
+
+function scuberGreetingForFeet (Feet) { 
+  if (Feet <= 400){
+    return ('This one is on me!');
+  }
+
+   else if (Feet >2500) {
+    return ('No can do.');
+  }
+
+  else if (Feet >= 2000) {
+    return ('I will gladly take your thirty bucks.');
+  } 
+  
+}
+
+
+function ternaryCheckCity(city) {
+let answer = city == "NYC" ? "Ok, sounds good." : "No go.";
+return answer;
+}
+
+
+function switchOnCharmFromTip (generosity) {
+switch (generosity) {
+  case  "generous":
+  return ("Thank you so much.");
+  break;
+  case "not as generous":
+  return("Thank you.");
+  break;
+  default:
+  return ("Bye.");
+}
+
+}
+
+
+
+
+
+
+
+
